41e30683dcceec7a15f92bdec4e12fcc0d859329
Sebastian Padilla <sebastianpadilla@ascbomacmini80.local>
2025-06-18 08:58:52 -0400

    ref: change switch statement with better readability

--- src/utils/getTabIcon.tsx
+++ src/utils/getTabIcon.tsx
@@ -1,4 +1,4 @@
-import {JSX} from 'react';
+import { JSX } from 'react';
 import Octicons from 'react-native-vector-icons/Octicons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Foundation from 'react-native-vector-icons/Foundation';
@@ -9,28 +9,14 @@ export const getTabIcon = (
   color: string,
   size: number,
 ): JSX.Element => {
-  switch (routeName) {
-    case 'Home':
-      return focused ? <Foundation name="home" size={size} color={color} /> : <Octicons name="home" size={size} color={color} />;
-    case 'Search':
-      return <Octicons name="search" size={size} color={color} />;
-    case 'WishList':
-      return (
-        <Ionicons
-          name={focused ? 'bookmark' : 'bookmark-outline'}
-          size={size}
-          color={color}
-        />
-      );
-    case 'Profile':
-      return (
-        <Ionicons
-          name={focused ? 'person' : 'person-outline'}
-          size={size}
-          color={color}
-        />
-      );
-    default:
-      return <Foundation name="record" size={size} color={color} />;
-  }
+  const routeObject: Record<string, JSX.Element> = {
+    Home: focused
+      ? <Foundation name="home" size={size} color={color} />
+      : <Octicons name="home" size={size} color={color} />,
+    Search: <Octicons name="search" size={size} color={color} />,
+    WishList: <Ionicons name={focused ? 'bookmark' : 'bookmark-outline'} size={size} color={color} />,
+    Profile: <Ionicons name={focused ? 'person' : 'person-outline'} size={size} color={color} />,
+  };
+
+  return routeObject[routeName] ?? <Foundation name="record" size={size} color={color} />;
 };
