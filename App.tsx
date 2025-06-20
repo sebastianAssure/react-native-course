0cf113bb1ddb1bbeea6e35ddb5657aeb7820e91b
Sebastian Padilla <sebastianpadilla@ascbomacmini80.local>
2025-06-20 18:30:07 -0400

    feat: add seeMore screen and add StackNavigation

--- App.tsx
+++ App.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { BottomTabsNavigator } from './src/navigation/BottomTabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SeeMore } from './src/screens/SeeMore';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
       <NavigationContainer>
-        <BottomTabsNavigator />
+        <Stack.Navigator
+          screenOptions={({}) => ({
+            headerShown: true,
+          })}>
+          <Stack.Screen name="Main" component={BottomTabsNavigator} />
+          <Stack.Screen name="SeeMore" component={SeeMore} />
+        </Stack.Navigator>
       </NavigationContainer>
     </View>
   );
